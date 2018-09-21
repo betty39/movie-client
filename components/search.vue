f651540c1b36e69f08b4a1fe08fe51a38e90296f
haha <14301133@bjtu.edu.cn>
2018-09-21 20:21:59 +0800

    更改参数传递错误

--- components/search.vue
+++ components/search.vue
@@ -36,7 +36,7 @@ export default {
     },
     methods: {
         changeSelect(item) {
-          let path = "/movie/" + item.movieid;
+          let path = "/movie/" + item;
           this.$router.push({path: path})
         },
         async searchRemoteInfo(query) {
